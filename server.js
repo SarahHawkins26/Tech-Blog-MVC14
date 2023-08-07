565fcb1522d07f8257ea0da6e08245b1c125db8f
Sarah <sarahhawkins26@gmail.com>
2023-08-07 14:02:40 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+//import express
+const express = require('express')
+//import connection to the database
+const sequelize = require('./config/connection')
+
+//set up the Express app
+const app = express()
+const PORT = process.env.PORT || 3001
+
+//connect to the database before starting the Express server
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}`);
+    });
+});
